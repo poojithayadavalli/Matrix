8f8d0c5365275d9980708263ff9eb11c69b79d00
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-19 21:31:52 +0530

    Create min steps to bring all ele to one cell.py

---
+++ min steps to bring all ele to one cell.py
@@ -0,0 +1,20 @@
+"""
+Given a matrix mat[][], pair of indices X and Y, the task is to find the number of moves to bring all the non-zero elements of the matrix to the given cell at (X, Y). 
+ 
+
+A move consists of moving an element at any cell to its four directional adjacent cells i.e., left, right, top, bottom. 
+ 
+
+Examples: 
+ 
+
+Input: mat[][] = {{1, 0}, {1, 0}}, X = 1, Y = 1 
+Output: 3 
+Explanation: 
+Moves required => 
+For Index (0, 0) => 2 
+For Index (1, 0) => 1 
+Total moves required = 3
+Input: mat[][] = {{1, 0, 1, 0}, {1, 1, 0, 1}, {0, 0, 1, 0}}, X = 1, Y = 3 
+Output: 13 
+"""
