0b2bf734268575c61ef5f45699724a17dae5da53
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-19 21:13:00 +0530

    Create print all paths.py

---
+++ print all paths.py
@@ -0,0 +1,15 @@
+"""
+The problem is to print all the possible paths from top left to bottom right of a mXn matrix with the constraints that from each cell you can either move only to right or down.
+Examples :
+
+Input : 1 2 3
+        4 5 6
+Output : 1 4 5 6
+         1 2 5 6
+         1 2 3 6
+
+Input : 1 2 
+        3 4
+Output : 1 2 4
+         1 3 4
+"""
