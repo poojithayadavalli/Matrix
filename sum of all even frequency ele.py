a4ed3a0f23c741fe4388e9219fd0c777f26e5a4b
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-19 21:39:23 +0530

    Create sum of all even frequency ele.py

---
+++ sum of all even frequency ele.py
@@ -0,0 +1,17 @@
+"""
+Given a NxM matrix of integers containing duplicate elements. The task is to find the sum of all even occurring elements in the given matrix. That is the sum of all such elements whose frequency is even in the matrix.
+
+Examples:
+
+Input : mat[] = {{1, 1, 2},
+                {2, 3, 3},
+                {4, 5, 3}}
+Output : 18
+The even occurring elements are 1, 2 and their number
+of occurrences are 2, 2 respectively. Therefore,
+sum = 1+1+2+2 = 6.
+
+Input : mat[] = {{10, 20},
+                 {40, 40}}
+Output : 80
+"""
