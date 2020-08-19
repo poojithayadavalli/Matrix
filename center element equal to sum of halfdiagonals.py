97eb943f7b05b95af7a824eace4c2eb86b0d3039
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-19 21:01:57 +0530

    Create center element equal to sum of halfdiagonals.py

---
+++ center element equal to sum of halfdiagonals.py
@@ -0,0 +1,22 @@
+"""
+Given a matrix of odd order i.e(5*5). Task is to check if the center element of the matrix is equal to the individual sum of all the half diagonals.
+
+
+
+Examples:
+
+Input : mat[][] = {   2   9   1   4  -2
+                      6   7   2  11   4
+                      4    2  9   2   4
+                      1   9   2    4  4
+                      0   2   4    2  5 } 
+Output :Yes
+Explanation : 
+Sum of Half Diagonal 1 = 2 + 7 = 9
+Sum of Half Diagonal 2 = 9 + 0 = 9
+Sum of Half Diagonal 3 = 11 + -2 = 9
+Sum of Half Diagonal 4 = 5 + 4 = 9
+
+Here, All the sums equal to the center element
+that is mat[2][2] = 9
+"""
