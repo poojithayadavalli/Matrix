b010fbcce0b9e4f0f7d7cb203a09c06ff001e203
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-19 21:36:55 +0530

    Create sum of all min frequency ele.py

---
+++ sum of all min frequency ele.py
@@ -0,0 +1,17 @@
+"""
+Given a NxM matrix of integers containing duplicate elements. The task is to find the sum of all minimum occurring elements in the given matrix. That is the sum of all such elements whose frequency is even in the matrix.
+
+Examples:
+
+Input : mat[] = {{1, 1, 2},
+                {2, 3, 3},
+                {4, 5, 3}}
+Output : 9
+The min occurring elements are 4, 5 and they 
+occurs only 1 time.
+Therefore, sum = 4+5 = 9
+
+Input : mat[] = {{10, 20},
+                 {40, 40}}
+Output : 30
+"""
