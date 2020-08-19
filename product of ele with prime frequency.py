5ac8fd0c82d66f157e377a3a5d7a5e269f6590db
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-19 21:44:03 +0530

    Create product of ele with prime frequency.py

---
+++ product of ele with prime frequency.py
@@ -0,0 +1,15 @@
+"""
+Given an array arr[] of N elements, the task is to find the product of the elements which have prime frequencies in the array. Since, the product can be large so print the product modulo 109 + 7. Note that 1 is neither prime nor composite.
+
+Examples:
+
+Input: arr[] = {5, 4, 6, 5, 4, 6}
+Output: 120
+All the elements appear 2 times which is a prime
+So, 5 * 4 * 6 = 120
+
+Input: arr[] = {1, 2, 3, 3, 2, 3, 2, 3, 3}
+Output: 6
+Only 2 and 3 appears prime number of times i.e. 3 and 5 respectively.
+So, 2 * 3 = 6
+"""
