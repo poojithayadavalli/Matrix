4a383e07ce1833265e299923c2afa158b1db22ea
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-19 21:50:14 +0530

    Create lower hessenberg matrix.py

---
+++ lower hessenberg matrix.py
@@ -0,0 +1,20 @@
+"""
+Given a matrix of order NxN, Find the minimum number of steps to convert given matrix into Lower Hessenberg matrix. In each step, the only operation allowed is to decrease or increase any element value by 1.
+
+Examples:
+
+Input: mat[][] = {
+{1, 2, 8},
+{1, 3, 4},
+{2, 3, 4}}
+Output: 8
+Decrease the element a[0][2] 8 times.
+Now the matrix is lower Hessenberg.
+
+Input: mat[][] = {
+{9, 2, 5, 5},
+{12, 3, 4, 5},
+{13, -3, 4, 2},
+{-1, 10, 1, 4}}
+Output: 15
+"""
