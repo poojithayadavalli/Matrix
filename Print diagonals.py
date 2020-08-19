11b85c832b7777e4d641e9dbc93d4b75c5a3f2e7
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-19 21:05:33 +0530

    Create Print diagonals.py

---
+++ Print diagonals.py
@@ -0,0 +1,24 @@
+"""
+Given a 2D square matrix, the task is to print the Principal and Secondary diagonals of this matrix in O(N) time complexity. For O(N2) time, please refer this article.
+
+Examples :
+
+Input: 
+4
+1 2 3 4
+4 3 2 1
+7 8 9 6
+6 5 4 3
+Output:
+Principal Diagonal: 1, 3, 9, 3
+Secondary Diagonal: 4, 2, 8, 6
+
+Input:
+3
+1 1 1
+1 1 1
+1 1 1
+Output:
+Principal Diagonal: 1, 1, 1
+Secondary Diagonal: 1, 1, 1
+"""
