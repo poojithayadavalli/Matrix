658e1f499994be2ea51a57276e212601d5d889be
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-19 21:10:25 +0530

    Create sum of all subsquares.py

---
+++ sum of all subsquares.py
@@ -0,0 +1,28 @@
+"""
+Given an n x n square matrix, find sum of all sub-squares of size k x k where k is smaller than or equal to n.
+Examples :
+
+Input:
+n = 5, k = 3
+arr[][] = { {1, 1, 1, 1, 1},
+            {2, 2, 2, 2, 2},
+            {3, 3, 3, 3, 3},
+            {4, 4, 4, 4, 4},
+            {5, 5, 5, 5, 5},
+         };
+Output:
+       18  18  18
+       27  27  27
+       36  36  36
+
+
+Input:
+n = 3, k = 2
+arr[][] = { {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+         };
+Output:
+       12  16
+       24  28
+"""
