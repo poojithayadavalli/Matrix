d1665dd77fc696b84291c65ab2c7236892fb5cb8
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-19 21:41:12 +0530

    Create count elements with value equal to frequency.py

---
+++ count elements with value equal to frequency.py
@@ -0,0 +1,15 @@
+"""
+Given an array of integers arr[] of size N, the task is to count all the elements of the array which have a frequency equals to its value.
+
+Examples:
+
+Input: arr[] = {3, 2, 2, 3, 4, 3}
+Output: 2
+Frequency of element 2 is 2
+Frequency of element 3 is 3
+Frequency of element 4 is 1
+2 and 3 are elements which have same frequency as it’s value
+
+Input: arr[] = {1, 2, 3, 4, 5, 6}
+Output: 1
+"""
