0b7244485c725e4c172f1a66d5a6d9385b74bc89
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-20 11:47:53 +0530

    Update sum of all min frequency ele.py

--- sum of all min frequency ele.py
+++ sum of all min frequency ele.py
@@ -1,15 +1,13 @@
 """
 Given a NxM matrix of integers containing duplicate elements. 
-
-The task is to find the sum of all even occurring elements in the given matrix.
-
-That is the sum of all such elements whose frequency is even in the matrix.
+The task is to find the sum of all minimum occurring elements in the given matrix.
+That is the sum of all such elements whose frequency is minimum in the matrix.
 
 Input:
 Firstline indicates integer N and integer M
 Next N lines contains elements of matrix.
 Output:
-print sum of all elements with even frequency
+print sum of all elements with min frequency
 
 Examples:
 
@@ -18,18 +16,18 @@ Input :
 1 1 2
 2 3 3
 4 5 3
-Output :
-6
-The even occurring elements are 1,2 and they 
-occurs only 2 times.
-Therefore, sum = 1*2+2*2 = 6
+Output : 
+9
+The even occurring elements are 4,5 and their number
+of occurrences are 1,1 respectively. Therefore,
+sum = 4+5 = 9.
 
 Input :
 2 2
 10 20
 40 40
 Output : 
-80
+30
 Input:
 4 4
 1 2 3 4
@@ -49,26 +47,30 @@ Input:
 1 1
 2 3
 Output:
-2
+5
 """
-M,N=map(int,input().split())
-def sumOddOccuring(arr):
-    mp = dict() 
-    for i in range(N): 
-        for j in range(M): 
-            if arr[i][j] in mp: 
-                mp[arr[i][j]] += 1
+import sys
+def sumMinOccuring(mat): 
+    n,m=len(mat),len(mat[0]) 
+    _map={} 
+    for i in range(n): 
+        for j in range(m): 
+            d=mat[i][j] 
+            if d in _map: 
+                _map[d]=_map.get(d)+1
             else: 
-                mp[arr[i][j]] = 1
-    s = 0
-    for i in mp: 
-        if mp[i] % 2 == 0: 
-            x = mp[i] 
-            s += i * mp[i] 
-  
-    return s 
+                _map[d]=1
+    _sum,minFreq=0,sys.maxsize 
+    for i in _map: 
+        minFreq=min(minFreq,_map.get(i))
+    for i in range(n): 
+        for j in range(m): 
+            if _map.get(mat[i][j])==minFreq: 
+                _sum+=mat[i][j] 
+      
+    return _sum 
+n,m=map(int,input().split())
 mat=[]
-for i in range(M):
+for i in range(n):
     mat.append(list(map(int,input().split())))
-print(sumOddOccuring(mat))
-
+print(sumMinOccuring(mat))
