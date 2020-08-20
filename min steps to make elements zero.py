229299dfff99884ff7506c9cb71c0e544e6b4597
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-20 10:12:55 +0530

    Create min steps to make elements zero.py

---
+++ min steps to make elements zero.py
@@ -0,0 +1,17 @@
+"""
+Given an array arr[] of positive integers, the task is to find the minimum steps to reduce all the elements to 0. In a single step, -1 can be added to all the non-zero elements of the array at the same time.
+
+Examples:
+
+Input: arr[] = {1, 5, 6}
+Output: 6
+Operation 1: arr[] = {0, 4, 5}
+Operation 2: arr[] = {0, 3, 4}
+Operation 3: arr[] = {0, 2, 3}
+Operation 4: arr[] = {0, 1, 2}
+Operation 5: arr[] = {0, 0, 1}
+Operation 6: arr[] = {0, 0, 0}
+
+Input: arr[] = {1, 1}
+Output: 1
+"""
